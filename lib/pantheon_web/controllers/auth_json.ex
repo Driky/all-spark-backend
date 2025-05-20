7bf53d4c432876b988dbcd78a60208e36148c7e2
Cedric Chatelain <cedric.chatelain@protonmail.com>
2025-05-19 21:00:58 -0400

    feat(auth): create JSON view for auth responses
    - Add AuthJSON view for formatting authentication responses
    - Implement token/1 for authentication token responses
    - Implement message/1 for simple message responses
    - Add tests for both rendering functions

---
+++ lib/pantheon_web/controllers/auth_json.ex
@@ -0,0 +1,24 @@
+defmodule PantheonWeb.AuthJSON do
+  @moduledoc """
+  JSON view for auth responses.
+  """
+
+  @doc """
+  Renders token data.
+  """
+  def token(%{data: data}) do
+    %{data: %{
+      token: data.token,
+      user_id: data.user_id
+    }}
+  end
+
+  @doc """
+  Renders message data.
+  """
+  def message(%{data: data}) do
+    %{data: %{
+      message: data.message
+    }}
+  end
+end
